994fa5fd6b342487ebe939b17d74fa50ced7bfe1
garciaFabricio <77011296+garciaFabricio@users.noreply.github.com>
2021-04-01 08:24:49 -0300

    endereço atualizado

--- javaRmi/src/rmi/RmiClientStart.java
+++ javaRmi/src/rmi/RmiClientStart.java
@@ -10,7 +10,7 @@ import java.rmi.RemoteException;
 public class RmiClientStart {
 
 	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
-		HelloService service = (HelloService) Naming.lookup("rmi://localhost:5099/hello");
+		HelloService service = (HelloService) Naming.lookup("rmi://189.48.80.221:5099/hello");
 		System.out.println(service.echo("hey"));
 
 	}
