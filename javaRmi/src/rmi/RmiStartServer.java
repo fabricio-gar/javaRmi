70a814509061b6367e0ba3a4e3a7cdb48c1e4602
garciaFabricio <77011296+garciaFabricio@users.noreply.github.com>
2021-04-03 00:17:24 -0300

    commit

--- javaRmi/src/rmi/RmiStartServer.java
+++ javaRmi/src/rmi/RmiStartServer.java
@@ -9,21 +9,21 @@ import java.rmi.registry.Registry;
 public class RmiStartServer {
 
 	public static void main(String[] args) throws RemoteException {
-		
+		System.setProperty("java.rmi.server.hostname","192.168.0.70");
 
-		Registry registry = LocateRegistry.createRegistry(5099);
+		Registry registry = LocateRegistry.createRegistry(1099);
 		
 		
-		Registry registry1 = LocateRegistry.getRegistry(50100);
+		Registry registry1 = LocateRegistry.createRegistry(1100);
 		registry.rebind("hello", new HelloServant());
 		registry1.rebind("utils", new UtilsServant());
 	
 		Pessoa pessoa1 = new Pessoa("fabricio@ufpa.br", "Fabricio",
-				"Garcia", "Belém", "Engenharia da Computação",
-				"Programação Java", "Desenvolvimento java 3 anos");
+				"Garcia", "BelÃ©m", "Engenharia da ComputaÃ§Ã£o",
+				"ProgramaÃ§Ã£o Java", "Desenvolvimento java 3 anos");
 		Pessoa pessoa2 = new Pessoa("daniel@ufpa.br", "Daniel",
-				"Faroas", "Belém", "Engenharia da Computação",
-				"Programação Android", "Desenvolvimento Kotlin 1 ano");
+				"Faroas", "BelÃ©m", "Engenharia da ComputaÃ§Ã£o",
+				"ProgramaÃ§Ã£o Android", "Desenvolvimento Kotlin 1 ano");
 		Pessoas.pessoas.add(pessoa1);
 		Pessoas.pessoas.add(pessoa2);
 	}
